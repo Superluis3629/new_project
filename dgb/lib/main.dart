752967ccb940595b9f2b34801e08688029c8c15f
Superluis3629 <108126524+Superluis3629@users.noreply.github.com>
2022-08-05 14:08:58 +0700

    Update main.dart

--- dgb/lib/main.dart
+++ dgb/lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Hello',
+      title: 'Hello I am Louis',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
